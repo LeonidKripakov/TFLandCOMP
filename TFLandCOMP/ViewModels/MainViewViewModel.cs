92dfe4b6d5b42e45e68136dd7297bed34c044f50
LeonidKripakov <leonidkripakov@gmail.com>
2025-05-05 09:32:29 +0700

    Update MainViewViewModel.cs

--- TFLandCOMP/ViewModels/MainViewViewModel.cs
+++ TFLandCOMP/ViewModels/MainViewViewModel.cs
@@ -33,33 +33,30 @@ namespace TFLandCOMP.ViewModels
         private void RunScan()
         {
             Errors.Clear();
-
-            if (string.IsNullOrWhiteSpace(InputText))
-                return;
-
             Lexer lexer = new Lexer();
-            Parser parser = new Parser();
-
             var tokens = lexer.Lex(InputText);
-            var parseErrors = parser.ParseDeclarations(tokens, InputText);
 
-            if (parseErrors.Count == 0)
+            if (tokens.Count == 0)
             {
                 Errors.Add(new ErrorDetail
                 {
-                    ErrorCode = "OK",
-                    ErrorMessage = "Ошибок не обнаружено",
+                    ErrorCode = "EMPTY",
+                    ErrorMessage = "Лексемы не найдены",
                     Position = ""
                 });
             }
             else
             {
-                foreach (var error in parseErrors)
+                foreach (var token in tokens)
                 {
-                    Errors.Add(error);
+                    Errors.Add(new ErrorDetail
+                    {
+                        ErrorCode = token.Type.ToString(),
+                        ErrorMessage = $"Значение: '{token.Value}'",
+                        Position = $"Позиция: {token.StartIndex}"
+                    });
                 }
             }
         }
-
     }
 }
